98b4388a1f06ecebc29a087b25fd3156c0e1af84
Andrea <andrea.aselli001@studenti.uniparthenope.it>
2023-06-07 18:32:58 +0200

    SECONDA MODIFICA

--- app/src/main/java/com/example/prova/MainActivity.kt
+++ app/src/main/java/com/example/prova/MainActivity.kt
@@ -8,4 +8,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    <Button
+    android:id="@+id/button0"
+    android:layout_width="0dp"
+    android:layout_height="wrap_content"
+    android:layout_columnWeight="1"
+    android:layout_rowWeight="1"
+    android:text="0"
+    android:textSize="24sp"/>
 }
